e8abce095584a0e60848fd12b9ac85d016f3bfce
Ekeyangwen <ekeyangwen@gmail.com>
2021-06-10 16:56:29 +0200

    Validation des champs

--- modal.js
+++ modal.js
@@ -12,13 +12,14 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeCross = document.querySelectorAll(".close"); //création d'une constante close
-const submitBtn = document.querySelectorAll("btn-submit");
+const submitBtn = document.getElementById("button");
 const fnElem = document.getElementById("first");
 const lastElem = document.getElementById("last");
 const mailElem = document.getElementById("email");
 const birthElem = document.getElementById("birthdate");
 const qtityElem = document.getElementById("quantity");
 const cityElem = document.getElementById("city");
+const form = document.getElementsByClassName("formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -38,27 +39,32 @@ function closeModal() {
 }
 
 //test submit event
-submitBtn.forEach((btn) => btn.addEventListener("click", first));
-submitBtn.forEach((btn) => btn.addEventListener("click", last));
-submitBtn.forEach((btn) => btn.addEventListener("click", mail));
-submitBtn.forEach((btn) => btn.addEventListener("click", quantity));
-submitBtn.forEach((btn) => btn.addEventListener("click", city));
+submitBtn.addEventListener("click", first);
+
+submitBtn.addEventListener("click", last);
+submitBtn.addEventListener("click", mail);
+submitBtn.addEventListener("click", birthdate);
+submitBtn.addEventListener("click", quantity);
+
+//test validate form
 
 function first(e) {
+  e.preventDefault();
   if (fnElem.value === "") {
     // on affiche un message
-    alert("Veuillez indiquer un nom");
-    return false;
+    alert("Veuillez indiquer un prénom");
+    // on entoure la case en rouge
+    form.style.border = "solid 2px red";
     // et on indique de ne pas envoyer le formulaire
+    return false;
   } else {
+    form.style.border = "";
     // les données sont ok, on peut envoyer le formulaire
     return true;
   }
 }
-
-//test validate form
-
-function last() {
+function last(e) {
+  e.preventDefault();
   if (lastElem.value === "") {
     // on affiche un message
     alert("Veuillez indiquer un nom");
@@ -70,7 +76,8 @@ function last() {
   }
 }
 
-function mail() {
+function mail(e) {
+  e.preventDefault();
   if (mailElem.value === "") {
     // on affiche un message
 
@@ -83,7 +90,8 @@ function mail() {
   }
 }
 
-function birthdate() {
+function birthdate(e) {
+  e.preventDefault();
   if (birthElem.value === "") {
     // on affiche un message
 
@@ -96,24 +104,12 @@ function birthdate() {
   }
 }
 
-function quantity() {
+function quantity(e) {
+  e.preventDefault();
   if (qtityElem.value === "") {
     // on affiche un message
 
     alert("Veuillez indiquer un nombre de tournois");
-    return false;
-    // et on indique de ne pas envoyer le formulaire
-  } else {
-    // les données sont ok, on peut envoyer le formulaire
-    return true;
-  }
-}
-
-function city() {
-  if (cityElem.value === "") {
-    // on affiche un message
-
-    alert("Veuillez indiquer une ville");
     return false;
     // et on indique de ne pas envoyer le formulaire
   } else {
