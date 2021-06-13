2126da7b795f91ccf4174d93394406926ba1cc63
Ekeyangwen <ekeyangwen@gmail.com>
2021-06-13 10:21:41 +0200

    Validation formulaire +styles

--- modal.js
+++ modal.js
@@ -13,13 +13,17 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeCross = document.querySelectorAll(".close"); //création d'une constante close
 const submitBtn = document.getElementById("button");
+const form = document.querySelector("#modalForm");
 const fnElem = document.getElementById("first");
 const lastElem = document.getElementById("last");
 const mailElem = document.getElementById("email");
 const birthElem = document.getElementById("birthdate");
-const qtityElem = document.getElementById("quantity");
-const cityElem = document.getElementById("city");
-const form = document.getElementsByClassName("formData");
+const tournoisElem = document.getElementById("quantity");
+const fnCheck = document.getElementById("fnCheck");
+const lastCheck = document.getElementById("lastCheck");
+const birthCheck = document.getElementById("birthCheck");
+const mailCheck = document.getElementById("mailCheck");
+const tournoisCheck = document.getElementById("qtityCheck");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -38,36 +42,35 @@ function closeModal() {
   modalbg.style.aria = "hidden";
 }
 
-//test submit event
+//Validation des champs
+firstRegExp = RegExp("^[a-zA-Z]+$[2,]", "g");
+
 submitBtn.addEventListener("click", first);
-
-submitBtn.addEventListener("click", last);
-submitBtn.addEventListener("click", mail);
-submitBtn.addEventListener("click", birthdate);
-submitBtn.addEventListener("click", quantity);
-
-//test validate form
-
 function first(e) {
   e.preventDefault();
   if (fnElem.value === "") {
+    fnCheck.innerHTML = "Veuillez indiquer un prénom";
+    fnCheck.style.fontSize = "10px";
+    fnCheck.style.color = "#FF4E60";
+
     // on affiche un message
-    alert("Veuillez indiquer un prénom");
-    // on entoure la case en rouge
-    form.style.border = "solid 2px red";
-    // et on indique de ne pas envoyer le formulaire
     return false;
+    // et on indique de ne pas envoyer le formulaire
   } else {
-    form.style.border = "";
     // les données sont ok, on peut envoyer le formulaire
     return true;
   }
 }
+
+submitBtn.addEventListener("click", last);
 function last(e) {
   e.preventDefault();
   if (lastElem.value === "") {
+    lastCheck.innerHTML = "Veuillez indiquer un nom";
+
+    lastCheck.style.fontSize = "10px";
+    lastCheck.style.color = "#FF4E60";
     // on affiche un message
-    alert("Veuillez indiquer un nom");
     return false;
     // et on indique de ne pas envoyer le formulaire
   } else {
@@ -76,12 +79,14 @@ function last(e) {
   }
 }
 
+submitBtn.addEventListener("click", mail);
 function mail(e) {
   e.preventDefault();
   if (mailElem.value === "") {
+    mailCheck.innerHTML = "Veuillez indiquer un email";
+    mailCheck.style.fontSize = "10px";
+    mailCheck.style.color = "#FF4E60";
     // on affiche un message
-
-    alert("Veuillez indiquer un email");
     return false;
     // et on indique de ne pas envoyer le formulaire
   } else {
@@ -90,12 +95,14 @@ function mail(e) {
   }
 }
 
+submitBtn.addEventListener("click", birthdate);
 function birthdate(e) {
   e.preventDefault();
   if (birthElem.value === "") {
+    birthCheck.innerHTML = "Veuillez indiquer une date de naissance";
+    birthCheck.style.fontSize = "10px";
+    birthCheck.style.color = "#FF4E60";
     // on affiche un message
-
-    alert("Veuillez indiquer une date de naissance");
     return false;
     // et on indique de ne pas envoyer le formulaire
   } else {
@@ -104,12 +111,14 @@ function birthdate(e) {
   }
 }
 
+submitBtn.addEventListener("click", quantity);
 function quantity(e) {
   e.preventDefault();
-  if (qtityElem.value === "") {
+  if (tournoisElem.value === "") {
+    tournoisCheck.innerHTML = "Veuillez indiquer un nombre de tournois";
+    tournoisCheck.style.fontSize = "10px";
+    tournoisCheck.style.color = "#FF4E60";
     // on affiche un message
-
-    alert("Veuillez indiquer un nombre de tournois");
     return false;
     // et on indique de ne pas envoyer le formulaire
   } else {
